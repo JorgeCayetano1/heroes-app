497783f82b4f5a4ff581373d6b60318b3ec10f14
Jorge Cayetano <cayetano.laboral@gmail.com>
2024-04-21 14:57:17 -0500

    section 15: finished

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,15 +3,25 @@ import {
   BrowserModule,
   provideClientHydration,
 } from '@angular/platform-browser';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
 import { SharedModule } from './shared/shared.module';
+import {
+  HttpClientModule,
+  provideHttpClient,
+  withFetch,
+} from '@angular/common/http';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, SharedModule],
-  providers: [provideClientHydration()],
+  providers: [
+    provideClientHydration(),
+    provideAnimationsAsync(),
+    provideHttpClient(withFetch()),
+  ],
   bootstrap: [AppComponent],
+  imports: [BrowserModule, HttpClientModule, AppRoutingModule, SharedModule],
 })
 export class AppModule {}
